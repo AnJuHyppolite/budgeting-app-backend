9360a280163f5f5a7e0f8ccbebef492a69a993b8
anjuhypp <anjuhypp@gmail.com>
2021-06-27 14:53:44 -0400

    update back-end

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,5 +1,5 @@
 const transactions = require("express").Router();
-const transaction = require("../models/transaction.js");
+// const transaction = require("../models/transaction.js");
 const transactionsArray = require("../models/transaction.js");
 
 //GET - read action - /transactions - get a list of all transactions
@@ -7,7 +7,7 @@ transactions.get("/", (req, res) => {
   res.json(transactionsArray);
 });
 
-//GET - read action - /transactions/:id - get an individual view (show one transaction)
+//GET - read action - /transactions/:id - get an individual view (SHOW one transaction)
 transactions.get("/:arrayIdx", (req, res) => {
   const { arrayIdx } = req.params;
   const transaction = transactionsArray[arrayIdx];
@@ -20,26 +20,35 @@ transactions.get("/:arrayIdx", (req, res) => {
 
 //POST - create action - /transactions - post has a body
 transactions.post("/", (req, res) => {
-    const { body } = req
-    transactionsArray.push(body)
-    const newIdx = transactionsArray.length - 1
-    res.json(transactionsArray[newIdx])
-})
+  const { body } = req;
+  transactionsArray.push(body);
+  const newIdx = transactionsArray.length - 1;
+  res.json(transactionsArray[newIdx]);
+});
 
 //DELETE - destroy action - /transactions/:id
 transactions.delete("/:arrayIdx", (req, res) => {
-  const { arrayIdx } = req.params
-  const deletedTransaction = transactionsArray.splice(arrayIdx, 1)
-  res.json(deletedTransaction[0])
-})
+  const { arrayIdx } = req.params;
+  const transaction = transactionsArray[arrayIdx];
+  const deletedTransaction = transactionsArray.splice(arrayIdx, 1);
+  if (transaction) {
+    res.json(deletedTransaction[0]);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 //PUT - update action - /transactions/:id - put has a body
 transactions.put("/:arrayIdx", (req, res) => {
-  const { arrayIdx } = req.params
-  const { body } = req
-  transactionsArray[arrayIdx] = body
-  res.json(transactionsArray[arrayIdx])
-})
-
+  const { arrayIdx } = req.params;
+  const { body } = req;
+  const transaction = transactionsArray[arrayIdx];
+  if (transaction) {
+    transaction = body;
+    res.json(transaction);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 module.exports = transactions;
